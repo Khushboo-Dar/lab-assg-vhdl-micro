9e0947d4dbdd49a87409d9d45bec0c493ab57609
Khushboo-Dar <khushboo_2022bite002@nitsri.ac.in>
2024-11-16 22:30:44 +0530

    added pgms

---
+++ and_gate/and_gate.vhd
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4feb198e4acadb9790055d1a53f9e6de72f74f694e02ae34f47c40c681148cef
+size 296
---
+++ and_gate/and_gate_tb.vhd
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:afe5c239a63bfc5ac8f99599f48ef6f4899b0c3b07b88a1e02d1d5ac1d1b26b5
+size 949
